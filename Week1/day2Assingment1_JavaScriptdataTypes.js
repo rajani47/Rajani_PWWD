67c1bb58f1d0df627371ad1a65f511143e2d1686
rajani47 <124551794+rajani47@users.noreply.github.com>
2026-08-18 19:07:15 +0530

    Add files via upload
    Week1 updated assignments are uploaded

---
+++ Week1/day2Assingment1_JavaScriptdataTypes.js
@@ -0,0 +1,21 @@
+/* Declare variables using `let` for different data types in JavaScript, and verify their types using `typeof`, including an uninitialized variable.
+Assignment Requirements:
+userName // String
+organization // String
+contactNumber // Number
+knowsAutomation // Boolean
+usesPlaywright // Undefined (not assigned) */
+
+let userName = 'Rajani'
+let organization = 'Amazon India'
+let contactNumber = 1234567890
+let knowsAutomation = false
+let usesPlaywright
+
+// Print the values and their data types in the console
+
+console.log('User Name is : ', userName, 'Data Type is : ', typeof userName);
+console.log('Organization is : ', organization, 'Data Type: ', typeof organization);
+console.log('Contact Number is : ', contactNumber, 'Data Type: ', typeof contactNumber);
+console.log('Knows Automation? : ', knowsAutomation, 'Data Type: ', typeof knowsAutomation);
+console.log('Uses Playwright? : ', usesPlaywright, 'Data Type: ', typeof usesPlaywright);
