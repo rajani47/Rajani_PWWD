d0e140dc2bacc0009a9eae40d5b44ef2da79229d
rajani47 <124551794+rajani47@users.noreply.github.com>
2026-08-21 18:11:20 +0530

    Launch Red Bus in Edge and Flipkart in Firefox
    Updated the previous code with correct code

--- Week2/day4Assignment1_LoginToWebPage.js
+++ Week2/day4Assignment1_LoginToWebPage.js
@@ -1,12 +1,32 @@
-import test, {chromium, Firefox} from '@playwright/test'
+import {test, chromium, firefox } from '@playwright/test'
 
-test('Launch Browser', async({})=>{
-const browser = await chromium.launch()
-const context = await browser.newContext()
- const page = await context.newPage()
- await page.goto('https://www.redbus.in')
- await page.waitForTimeout(5000)
- console.log(await page.title());
- console.log(await page.url());
+/* Your task is to launch two separate browser instances using Playwright:
+1. Load Red Bus in an Edge browser instance and verify the page title and URL.
+2. Load Flipkart in a Firefox browser instance and verify the page title and URL. */
 
+// Red Bus (Edge):o Load the home page and print the page title and current URL.
+test('Launch Red Bus app in Edge Browser', async()=>{
+
+    const browser = await chromium.launch({channel:'msedge',headless:false})
+    const context = await browser.newContext()
+    const page1 = await context.newPage()
+    await page1.goto('https://www.redbus.in')
+    const pageTitle = await page1.title()
+    const pageUrl = await page1.url()
+    console.log('Page title is : ' + pageTitle)
+    console.log('Current page URL is : ' + pageUrl)
+     await page1.waitForTimeout(2000)
+})
+
+//Flipkart (Firefox):Load the home page and print the page title and current URL
+test('Launch Flipcard in Firefox Browser', async()=>{
+    const browser = await firefox.launch({headless:false})
+    const context = await browser.newContext()
+    const page = await context.newPage()
+    await page.goto('https://www.flipkart.com')
+    const pageTitle = await page.title()
+    const pageUrl = await page.url()
+    console.log('Page title is : ' + pageTitle)
+    console.log('Current page URL is : ' + pageUrl)
+    await page.waitForTimeout(2000)
 })
